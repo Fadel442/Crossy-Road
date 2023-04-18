55b0161fc2b77b3f26825f9da3e0a7dfd8f46b81
BlueMoonZ <107695041+BlueMoon-Z@users.noreply.github.com>
2023-04-18 14:34:58 +0700

    tes

--- Assets/Doragon.cs
+++ Assets/Doragon.cs
@@ -9,11 +9,19 @@ public class Doragon : MonoBehaviour
 {
     [SerializeField, Range(0, 1)] float moveDuration = 0.1f;
     [SerializeField, Range(0, 1)] float jumpHeight = 0.5f;
+    [SerializeField] int leftMoveLimit;
+    [SerializeField] int rightMoveLimit;
+    [SerializeField] int backMoveLimit;
 
     public UnityEvent<Vector3> OnJumpEnd;
-    bool isMoving;
+    bool isDie;
     void Update()
     {
+        if (isDie)
+        {
+            return;
+        }
+
         if(DOTween.IsTweening(transform))
             return;
 
@@ -44,6 +52,16 @@ public class Doragon : MonoBehaviour
 
     private void Move(Vector3 direction)
     {
+        var targetPosition = transform.position + direction;
+
+        if (targetPosition.x < leftMoveLimit ||
+        targetPosition.x > rightMoveLimit ||
+        targetPosition.z < backMoveLimit ||
+        Tree.AllPositionSet.Contains(targetPosition))
+        {
+            targetPosition = transform.position;
+        }
+
         transform.DOJump(
             transform.position + direction, 
             jumpHeight, 1,  
@@ -53,8 +71,25 @@ public class Doragon : MonoBehaviour
         transform.forward = direction;
     }
 
+    public void UpdateMoveLimit(int horizontalSize, int backLimit)
+    {
+        leftMoveLimit = -horizontalSize / 2;
+        rightMoveLimit = horizontalSize / 2;
+        backMoveLimit = backLimit;
+    }
+
     private void BroadcastPositionOnJumpEnd()
     {
         OnJumpEnd.Invoke(transform.position); 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isDie == true)
+        {
+            return;
+        }
+        transform.DOScaleY(0.1f, 0.2f);
+        isDie = true;
     }
 }
